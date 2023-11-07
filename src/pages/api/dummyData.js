90aff6cda2425258a4da9d3eeeeba10032451026
auychat <auychatcharin@gmail.com>
2023-11-07 14:31:31 +0630

    Add dummyData.js

---
+++ src/pages/api/dummyData.js
@@ -0,0 +1,84 @@
+export const dummyData = {
+    "todos": [
+      {
+        "id": 1,
+        "task": "Buy groceries",
+        "description": "Milk, eggs, bread, and fruits",
+        "dueDate": "2023-11-15",
+        "priority": "High",
+        "completed": false
+      },
+      {
+        "id": 2,
+        "task": "Finish work report",
+        "description": "Complete the quarterly report for the boss",
+        "dueDate": "2023-11-10",
+        "priority": "Medium",
+        "completed": true
+      },
+      {
+        "id": 3,
+        "task": "Gym workout",
+        "description": "Chest and triceps day",
+        "dueDate": "2023-11-11",
+        "priority": "Low",
+        "completed": false
+      },
+      {
+        "id": 4,
+        "task": "Call mom",
+        "description": "Check in and chat with her",
+        "dueDate": "2023-11-14",
+        "priority": "Medium",
+        "completed": false
+      },
+      {
+        "id": 5,
+        "task": "Pay bills",
+        "description": "Electricity, water, and internet bills",
+        "dueDate": "2023-11-09",
+        "priority": "High",
+        "completed": true
+      },
+      {
+        "id": 6,
+        "task": "Read a book",
+        "description": "Chapter 7 of 'The Great Gatsby'",
+        "dueDate": "2023-11-13",
+        "priority": "Low",
+        "completed": false
+      },
+      {
+        "id": 7,
+        "task": "Plan vacation",
+        "description": "Research and decide on a destination",
+        "dueDate": "2023-11-18",
+        "priority": "High",
+        "completed": false
+      },
+      {
+        "id": 8,
+        "task": "Write blog post",
+        "description": "Topic: 'Top 10 Web Development Frameworks'",
+        "dueDate": "2023-11-12",
+        "priority": "Medium",
+        "completed": true
+      },
+      {
+        "id": 9,
+        "task": "Fix leaking faucet",
+        "description": "Buy replacement parts and fix the faucet",
+        "dueDate": "2023-11-16",
+        "priority": "Low",
+        "completed": false
+      },
+      {
+        "id": 10,
+        "task": "Learn a new recipe",
+        "description": "Try making homemade pizza from scratch",
+        "dueDate": "2023-11-17",
+        "priority": "Medium",
+        "completed": false
+      }
+    ]
+  }
